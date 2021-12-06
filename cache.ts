8b59fe5d0d5c8dce243a344ea2d98fd16f9ca942
venashial <venashial.levo@aleeas.com>
2021-12-05 18:55:40 -0800

    Fix cache not being filled

--- cache.ts
+++ cache.ts
@@ -1,9 +1,9 @@
 import {Assignment} from './types.d.ts';
 import {fetchAssignments} from "./api.ts";
 
-export const cache: Assignment[] = []
+export let cache: Assignment[] = []
 
 export async function fillCache(): Promise<void> {
-    cache.concat(await fetchAssignments())
-    console.log('Cache filled.')
+    cache = cache.concat(await fetchAssignments())
+    console.log(`Cache filled with ${cache.length} assignments.`)
 }
